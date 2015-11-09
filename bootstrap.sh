c2d1b97ca1a7f8f4166d48fb4964d96e9b1d8568
Daniel Cloud <daniel@danielcloud.org>
2015-11-09 10:31:27 -0500

    Add intial version of bootstrap.sh based on mathiasbynens/dotfiles"

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+    rsync --exclude ".git/"  --filter ". rsync.rules" -avh --no-perms --dry-run . ~;
+    source ~/.zsrhrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
