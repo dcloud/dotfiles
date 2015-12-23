68731d0b286d8f0c9afc2b755aee0f8b0f40b1b1
Daniel Cloud <daniel@danielcloud.org>
2015-12-23 15:53:38 -0500

    Disable virtualenv prompt override. See RPS1 in zsh theme.

--- zsh/virtualenv.zsh
+++ zsh/virtualenv.zsh
@@ -4,6 +4,8 @@
 export PROJECT_HOME=$HOME/code/python
 # Set WORKON_HOME so virtualenvs have a home
 export WORKON_HOME=$HOME/.virtualenvs
+# Disable virtualenv prompt override so we can do custom RPS1 instead
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 # Virtualenvwrapper, lazily
 if [[ -f $BREW_PREFIX/bin/virtualenvwrapper.sh ]]; then
