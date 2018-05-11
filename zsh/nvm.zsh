28aec6b039abe44ffbf2e2506ff3845e3d96074d
Daniel Cloud <daniel@danielcloud.org>
2018-05-11 16:59:26 -0700

    Use homebrew to install and manage nvm.

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,5 +1,5 @@
 # Set up node version manager
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+NVM_BIN="/usr/local/opt/nvm/nvm.sh"
+[ -s $NVM_BIN ] && \. $NVM_BIN  # This loads nvm
