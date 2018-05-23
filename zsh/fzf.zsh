7b7565975fceaa52ba95d2533d6efdfecc748381
Daniel Cloud <daniel@danielcloud.org>
2018-05-23 09:38:29 -0700

    If we have fd installed, configure fzf to use that to respect gitignore.

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,7 @@
 if [[ -f ~/.fzf.zsh  ]]; then
     source ~/.fzf.zsh;
     alias vif='vim $(fzf)'
+    if command -v fd &> /dev/null; then
+        export FZF_DEFAULT_COMMAND='fd --type f';
+    fi
 fi
