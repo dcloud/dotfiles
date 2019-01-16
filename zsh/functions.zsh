a1b93712ea8ab1665e0943ae1a1ca5cfaf9f9c81
Daniel Cloud <daniel@danielcloud.org>
2019-01-16 12:18:39 -0500

    Zsh: Add function for what is running on a port. Thanks, coworkers.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,3 +11,15 @@ function pynit {
     fi
     touch ${DIR}/__init__.py
 }
+
+# What's running on a port
+function wop() {
+    cmd="lsof -i -P -n | grep LISTEN | grep :$1"
+    echo -e "\e[2mlsof -i -P -n | grep LISTEN | grep :$1\e[0m"
+    lsof -i -P -n | grep LISTEN | grep :$1
+    local exitcode=$?
+    if [ $exitcode -ne 0 ]; then
+        echo "Nothing listening on port $1";
+    fi
+    return $exitcode;
+}
