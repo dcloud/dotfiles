c8920a99807343a378da5c792aadd11f10d1c143
Daniel Cloud <daniel@danielcloud.org>
2019-11-08 16:15:37 -0500

    Starship prompt (#8)
    * Zsh/nvm: Use correct paths for homebrew-installed nvm
    
    * [Zsh]: Use/enable starship prompt.

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -9,10 +9,8 @@
 lazynvm() {
   unset -f nvm node npm npx
   export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-  if [ -f "$NVM_DIR/bash_completion" ]; then
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  fi
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
 }
 
 nvm() {
