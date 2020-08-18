2222d843ca8e95217bd6673ad48c625a0cec6dbe
Daniel Cloud <daniel@danielcloud.org>
2020-08-18 11:28:45 -0400

    Vim/fzf: Use +Files invocation for vif alias

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,7 +1,7 @@
 if [[ -f ~/.fzf.zsh  ]]; then
     export FZF_CTRL_R_OPTS="-e"
     source ~/.fzf.zsh;
-    alias vif='vim $(fzf)'
+    alias vif='vim +Files'
     if command -v fd &> /dev/null; then
         export FZF_DEFAULT_COMMAND='fd --type f';
     fi
