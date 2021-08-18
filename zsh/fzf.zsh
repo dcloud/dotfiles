8b95be3803e118a8845b93e6b9cbaa8def3fca46
Daniel Cloud <daniel@danielcloud.org>
2021-08-18 16:30:02 -0400

    Zsh: Update fzf setup script to not rely on a config file

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,5 @@
-if [[ -f ~/.fzf.zsh  ]]; then
+if command -v fzf &> /dev/null; then
     export FZF_CTRL_R_OPTS="-e"
-    source ~/.fzf.zsh;
     alias vif='vim +Files'
     if command -v fd &> /dev/null; then
         export FZF_DEFAULT_COMMAND='fd --type f';
