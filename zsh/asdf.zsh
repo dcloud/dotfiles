adea59851bda11e7ae8755be8d048931a112d22a
Daniel Cloud <daniel@danielcloud.org>
2021-11-10 15:35:53 -0500

    Switch to asdf over nvm and rbenv

---
+++ zsh/asdf.zsh
@@ -0,0 +1,2 @@
+# https://asdf-vm.com
+. $(brew --prefix asdf)/libexec/asdf.sh
--- zsh/rbenv.zsh
+++
@@ -1,7 +0,0 @@
-# rbenv
-
-if hash rbenv 2>/dev/null; then
-    export RBENV_ROOT=$BREW_PREFIX/opt/rbenv
-    eval "$(rbenv init --no-rehash - zsh)"
-fi
-
