c3d217a3568e2d23cebb4bc1868572b81733bd96
Daniel Cloud <daniel@danielcloud.org>
2023-07-13 11:48:14 -0400

    Switch to asdf-java, set up JAVA_HOME

---
+++ zsh/asdf-java.zsh
@@ -0,0 +1,9 @@
+# https://github.com/halcyon/asdf-java
+
+local SCRIPT="$HOME/.asdf/plugins/java/set-java-home.zsh"
+
+if [[ -f "$SCRIPT"  ]]; then
+    source "$SCRIPT"
+fi
+
+
--- zsh/jenv.zsh
+++
@@ -1,8 +0,0 @@
-# jenv for Java
-
-if hash jenv 2>/dev/null; then
-    export JENV_ROOT=/usr/local/opt/jenv
-    eval "$(jenv init -)";
-fi
-
-
