a008f4d38dc08f021d69567d309bf134454fbe2e
Daniel Cloud <daniel@danielcloud.org>
2024-04-06 21:29:38 -0400

    Nvim: check for lsp executable before configuring

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -9,50 +9,58 @@ for _, value in pairs(server_list ) do
   }
 end
 
-nvim_lsp['gopls'].setup{
-  cmd = {'gopls'},
-  capabilities = capabilities,
-  settings = {
-    gopls = {
-      experimentalPostfixCompletions = true,
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
+if vim.fn.executable('gopls') == 1 then
+    nvim_lsp['gopls'].setup{
+    cmd = {'gopls'},
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+        experimentalPostfixCompletions = true,
+        analyses = {
+            unusedparams = true,
+            shadow = true,
+        },
+        staticcheck = true,
+        },
     },
-  },
-  init_options = {
-    usePlaceholders = true,
-  }
-}
+    init_options = {
+        usePlaceholders = true,
+    }
+    }
+end
 
-nvim_lsp['lua_ls'].setup {
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
-          },
-          -- Make the server aware of Neovim runtime files
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
+if vim.fn.executable('sourcekit-lsp') == 1 then
+    nvim_lsp['sourcekit'].setup{}
+end
+
+if vim.fn.executable('lua-language-server') == 1 then
+    nvim_lsp['lua_ls'].setup {
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+            Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                vim.env.VIMRUNTIME
+                }
             }
-          }
-        }
-      })
+            }
+        })
 
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end
+        return true
     end
-    return true
-  end
-}
+    }
+end
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
