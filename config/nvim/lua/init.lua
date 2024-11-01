b0632f5f2d537f23005f8a4976e290716d4aa030
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2024-11-01 14:12:14 -0400

    Neovim: keymaps for persistence session management

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -7,5 +7,8 @@ require("config.lazy")
 -- indent-blankline config
 require("config.ibl")
 
+-- session persistence
+require("config.persistence")
+
 -- set colorscheme after options
-vim.cmd('colorscheme rose-pine')
+vim.cmd("colorscheme rose-pine")
