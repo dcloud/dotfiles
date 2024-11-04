7946b9b72809fc83ceb1377feee7c86960460b34
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2024-11-04 14:55:21 -0500

    Neovim: Reorganize cmp.lua to reflect dependencies

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,18 @@
 return {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
-    "hrsh7th/vim-vsnip-integ",
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-vsnip",
+        },
+    },
+    {
+
+        "hrsh7th/vim-vsnip",
+        dependencies = {
+            "hrsh7th/vim-vsnip-integ",
+        },
+    },
 }
