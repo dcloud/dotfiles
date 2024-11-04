01876571ac802ded63f01bb4b5a07aba7be03761
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2024-11-04 14:54:53 -0500

    Neovim: nvim-lint to fill linting gaps

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -10,5 +10,8 @@ require("config.ibl")
 -- session persistence
 require("config.persistence")
 
+-- linters to supplement lsp
+require("config.lint")
+
 -- set colorscheme after options
 vim.cmd("colorscheme rose-pine")
