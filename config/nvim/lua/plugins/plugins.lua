01f69bca7ac7cfdc1c5ca4083e23e8589cd715ae
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-01-08 15:40:47 -0500

    Neovim: Give 'lint' its own plugin file, configure a la josean-dev

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,7 +1,6 @@
 return {
     "lewis6991/gitsigns.nvim",
     "mattn/emmet-vim",
-    "mfussenegger/nvim-lint",
     "neovim/nvim-lspconfig",
     "ray-x/go.nvim",
     { "rose-pine/neovim", name = "rose-pine", opts = { dim_inactive_windows = true } },
