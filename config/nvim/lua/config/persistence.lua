04f178df5ae136e8324865a4fad78298e92b286f
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-01-29 11:14:05 -0500

    Neovim: Add descriptions for persistence keymaps

--- config/nvim/lua/config/persistence.lua
+++ config/nvim/lua/config/persistence.lua
@@ -1,19 +1,19 @@
 -- load the session for the current directory
 vim.keymap.set("n", "<leader>qs", function()
     require("persistence").load()
-end)
+end, { desc = "Load session for the curent directory" })
 
 -- select a session to load
 vim.keymap.set("n", "<leader>qS", function()
     require("persistence").select()
-end)
+end, { desc = "Select a session to load" })
 
 -- load the last session
 vim.keymap.set("n", "<leader>ql", function()
     require("persistence").load({ last = true })
-end)
+end, { desc = "Load the last session" })
 
 -- stop Persistence => session won't be saved on exit
 vim.keymap.set("n", "<leader>qd", function()
     require("persistence").stop()
-end)
+end, { desc = "Stop persistence (session won't be saved)" })
