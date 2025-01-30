23ee0087969844db087671d7dda1734f91863c6d
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-01-30 15:38:20 -0500

    Nvim: Alter fzf-lua profile, keep keymaps I'm used to

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -3,7 +3,10 @@ return {
         "ibhagwan/fzf-lua",
         config = function()
             -- calling `setup` is optional for customization
-            require("fzf-lua").setup({ "fzf-vim" })
+            -- no_esc option for grep?
+            local fzf = require("fzf-lua")
+            fzf.setup({ "default" })
+            fzf.setup_fzfvim_cmds()
         end,
     },
 }
