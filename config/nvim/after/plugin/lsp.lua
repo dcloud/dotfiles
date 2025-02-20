bb3b513e24f006bc620eebfa33c7f43d97637db4
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-02-20 10:59:22 -0500

    Neovim: use ruby-lsp rather than rubocop. Adjust some keymaps

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local server_list = { "denols", "rubocop", "ruff", "rust_analyzer", "zls" }
+local server_list = { "denols", "ruby_lsp", "ruff", "rust_analyzer", "zls" }
 for _, value in pairs(server_list) do
     lspconfig[value].setup({
         capabilities = capabilities,
@@ -81,15 +81,21 @@ if vim.fn.executable("lua-language-server") == 1 then
     })
 end
 
-if vim.fn.executable("rubocop") == 1 then
-    lspconfig["rubocop"].setup({})
+if vim.fn.executable("ruby-lsp") == 1 then
+    lspconfig.ruby_lsp.setup({
+        init_options = {
+            formatter = "standard",
+            linters = { "standard" },
+        },
+    })
 end
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>ef", vim.diagnostic.open_float, { desc = "Open diagnostic in float" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>el", vim.diagnostic.setloclist, { desc = "Put diagnostic in loclist" })
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -107,16 +113,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set("n", "<space>wl", function()
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wl", function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, opts)
-        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<space>f", function()
+        vim.keymap.set("n", "<leader>f", function()
             vim.lsp.buf.format({ async = true })
         end, opts)
     end,
