78ab17f7bb7d59c1603163873cb3c0569ea00cde
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-04-16 10:32:18 -0400

    Neovim: nvim-lint ignore errors when running

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -1,24 +1,22 @@
 return {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        linters_by_ft = { ruby = { "rubocop" } },
+    },
     config = function()
         local lint = require("lint")
-
-        lint.linters_by_ft = {
-            ruby = { "rubocop" },
-        }
-
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
-                lint.try_lint()
+                lint.try_lint(nil, { ignore_errors = true })
             end,
         })
 
         vim.keymap.set("n", "<leader>l", function()
-            lint.try_lint()
+            lint.try_lint(nil, { ignore_errors = true })
         end, { desc = "Trigger linting for current file" })
     end,
 }
