a11473c6aa9a13f5f6da6fb35ced81f2d4bc9009
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-04-21 12:20:20 -0400

    Neovim: Set up ai-lualine to show AI activity

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,23 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         event = "ColorScheme",
-        opts = {
-            options = {
-                show_filename_only = false,
-                theme = "rose-pine",
-                icons_enabled = false,
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
-                lualine_y = { "copilot" },
-                lualine_z = { "progress", "location" },
-            },
-        },
+        opts = function()
+            local ai = require("lualine.components.ai-lualine")
+            return {
+                options = {
+                    show_filename_only = false,
+                    theme = "rose-pine",
+                    icons_enabled = false,
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { "filename" },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { ai },
+                    lualine_z = { "progress", "location" },
+                },
+            }
+        end,
     },
 }
