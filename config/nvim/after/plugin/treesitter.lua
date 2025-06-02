3282cd0a772ee21efadd986ac2ed676db9c6c5d9
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-06-02 14:53:08 -0400

    Neovim: Update treesitter to 'main' branch and change config to new version

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,18 +1,3 @@
---- treesitter config
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "embedded_template", "html", "javascript", "lua", "pkl", "vim", "vimdoc", "query" },
-    sync_install = false,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = { "lua" },
-    },
-    indent = {
-        enable = true,
-        disable = { "lua" },
-    },
-})
-
 -- treesitter folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,13 @@
-return { "nvim-treesitter/nvim-treesitter", branch = "master", build = ":TSUpdate" }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  config = function()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+
+    local ensure_installed = { "c", "embedded_template", "html", "javascript", "lua", "pkl", "vim", "vimdoc", "query" }
+    treesitter.install(ensure_installed)
+  end,
+  lazy = false,
+  build = ":TSUpdate",
+}
