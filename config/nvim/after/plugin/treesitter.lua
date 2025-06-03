51fd8a09a938a31ebee72e9f3244c8a83fdfd1bb
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-06-03 12:17:17 -0400

    Neovim: Migrate to nvim-treesitter main branch, attempt to autoload for supported filetypes.

--- config/nvim/after/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
--- treesitter folding
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
-
--- treesitter-embedded-template
-vim.treesitter.language.register("embedded_template", "ejs")
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,53 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   config = function()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
+    local nvim_ts = require("nvim-treesitter")
+    nvim_ts.setup()
 
-    local ensure_installed = { "c", "embedded_template", "html", "javascript", "lua", "pkl", "vim", "vimdoc", "query" }
-    treesitter.install(ensure_installed)
+    local ensure_installed = {
+      "c",
+      "embedded_template",
+      "gitcommit",
+      "gitignore",
+      "html",
+      "javascript",
+      "lua",
+      "pkl",
+      "python",
+      "query",
+      "sql",
+      "vim",
+      "vimdoc",
+    }
+    nvim_ts.install(ensure_installed)
+    vim.treesitter.language.register("embedded_template", "ejs")
+
+    -- Get available languages
+    local available_languages = nvim_ts.get_available()
+
+    -- Map languages to filetypes
+    local all_supported_filetypes = vim
+      .iter(available_languages)
+      :map(function(lang)
+        local filetypes = vim.treesitter.language.get_filetypes(lang)
+        return filetypes
+      end)
+      :flatten()
+      :totable()
+
+    -- print(table.concat(all_supported_filetypes, ", "))
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = all_supported_filetypes,
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
   lazy = false,
   build = ":TSUpdate",
