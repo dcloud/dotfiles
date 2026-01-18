07fb1beb1c987860b52c082549b2137e18f830c0
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2026-01-18 15:12:07 -0500

    Zsh: Update completions, check for extra zsh-completions

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,6 +1,9 @@
 # Completions
 
-#  npm completion
-if [ -f $BREW_PREFIX/etc/bash_completion.d/npm ]; then
-    . $BREW_PREFIX/etc/bash_completion.d/npm
+local BREW_PREFIX=$(brew --prefix);
+
+local zsh_completions="$(brew --prefix)/share/zsh-completions";
+#  Extra zsh completions
+if [ -d $zsh_completions ]; then
+    fpath=($zsh_completions $fpath)
 fi
