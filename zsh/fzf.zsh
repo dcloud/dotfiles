5f6485e13a5b4a5033876dec1b9674ad09c33580
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2026-03-03 11:36:35 -0500

    Zsh: fzf with 'bat' preview

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,6 @@
 if whence fzf &> /dev/null; then
     export FZF_CTRL_R_OPTS='--no-sort --exact';
+    export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always {}'"
     if whence fd &> /dev/null; then
         export FZF_DEFAULT_COMMAND='fd --type f';
     fi
