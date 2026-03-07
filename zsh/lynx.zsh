24b834a473be6805117e96d3cef78a380213a22e
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2026-03-07 12:47:29 -0500

    Zsh, lynx: remove `duck` script. Customize lynx to look better with rose-pine

--- zsh/lynx.zsh
+++ zsh/lynx.zsh
@@ -2,3 +2,7 @@ if [[ -e "$HOME/.config/lynx/lynx.cfg" ]] then
     export LYNX_CFG="$HOME/.config/lynx/lynx.cfg"
 fi
 
+if [[ -e "$HOME/.config/lynx/lynx.lss" ]] then
+    export LYNX_LSS="$HOME/.config/lynx/lynx.lss"
+fi
+
