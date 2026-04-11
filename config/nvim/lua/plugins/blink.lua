13ee4b18a5a0a2fb271e8c58c302a06c8fd5c5bf
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2026-04-11 13:31:31 -0400

    Neovim: enable suggested config. Blink wasn't suggesting buffers

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -15,13 +15,14 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    -- keymap = { preset = "default" },
+    keymap = { preset = "default" },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
-    -- sources = {
-    --   default = { "lsp", "path", "snippets", "buffer" },
-    -- },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
   },
   opts_extend = { "sources.default" },
 }
