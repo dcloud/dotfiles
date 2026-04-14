db654d668c6390a3195345a8102893f220b407b3
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2026-04-14 11:56:25 -0400

    Neovim: apparently missed vim-dadbod dependency

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -2,6 +2,6 @@ return {
   "lewis6991/gitsigns.nvim",
   "neovim/nvim-lspconfig",
   "tpope/vim-commentary",
-  "tpope/vim-dadbod",
+  { "tpope/vim-dadbod", dependencies = { "tpope/vim-dispatch" } },
   "tpope/vim-surround",
 }
